ce523a6e50cdb57801aac84bb9c4562100d2c213
Denis <waridesu@gmail.com>
2020-12-17 21:30:50 +0200

    #

--- sendForm.php
+++ sendForm.php
@@ -1,6 +1,6 @@
 <?php
-    $mail_to = 'max.yenin@gmail.com' . ', ';
-    $mail_to .= 'daewon-studio@yandex.ru';
+$mail_to = 'max.yenin@gmail.com' . ', ';
+$mail_to .= 'daewon-studio@yandex.ru';
 
     $name = $_POST['name'];
     $company = $_POST['company'];
@@ -16,27 +16,29 @@
     $body_message = 'From: ' . $name . "\r\n";
     $body_message .= 'company: ' . $company . "\r\n";
     $body_message .= 'Phone: ' . $phone . "\r\n";
-    $body_message .= 'E-mail: ' . $mail_from . "\r\n";
+    $body_message .= 'E-mail: ' . $email . "\r\n";
     $body_message .= 'amount: ' . $amount . "\r\n";
     $body_message .= 'duration: ' . $duration . "\r\n";
     $body_message .= 'formTax: ' . $formTax . "\r\n";
     $body_message .= 'formMonthly: ' . $formMonthly . "\r\n";
 
-    $headers = 'From: ' . $mail_from . "\r\n";
-    $headers .= 'Reply-To: ' . $mail_from . "\r\n";
+    $headers = 'From: ' . $name . "\r\n";
 
     $mail_sent = mail($mail_to, $subject, $body_message, $headers);
 
     if ($mail_sent == true){ ?>
         <script language="javascript" type="text/javascript">
-            document.querySelector('.send-success').style.display="block"
-  // window.location = 'index.html#formulaire';
-  // class to form hide
+            window.history.back();
+            window.addEventListener('DOMContentLoaded', function (event) {
+                    document.querySelector('.send-success').style.display='block'
+                    }
          </script>
     <?php } else { ?>
         <script language="javascript" type="text/javascript">
-            document.querySelector('.send-fail').style.display="block"
-  // window.location = 'index.html#formulaire';
+            window.history.back();
+            window.addEventListener('DOMContentLoaded', function (event) {
+                document.querySelector('.send-fail').style.display='block'
+            }
          </script>
     <?php
     }
